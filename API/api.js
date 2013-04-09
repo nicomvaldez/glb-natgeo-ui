802ccb13ade47a785ec6d03c4ddc395871135fa4
nicomvaldez <nicovaldez@ubuntu.ubuntu-domain>
2013-04-09 11:11:38 -0300

    commit 4

--- API/api.js
+++ API/api.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+ 
+var app = express();
+ 
+app.get('/topos', function(req, res) {
+    res.send([{name:'lucas'}, {name:'dami'}, {name:'eliana'}, {name:'nico'}]);
+});
+app.get('/topos/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+});
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');
