367504288e622d4bd8d11d57acea214c54cb0e2e
nicomvaldez <nicovaldez@ubuntu.ubuntu-domain>
2013-04-09 12:22:07 -0300

    adding jshint and watch grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,50 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: '<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    stylus: {
-      compile: {
-        options: {
-          paths: ['css/stylus/includes'],
-          import: ['variables']            
+    // Project configuration.
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+        uglify : {
+            options : {
+                banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build : {
+                src : '<%= pkg.name %>.js',
+                dest : 'build/<%= pkg.name %>.min.js'
+            }
         },
-        files: {
-          'css/styles.css': 'css/stylus/home.styl'
+        stylus : {
+            compile : {
+                options : {
+                    paths : ['css/stylus/includes'],
+                    import : ['variables']
+                },
+                files : {
+                    'css/styles.css' : 'css/stylus/home.styl'
+                }
+            }
+        },
+        jshint : {
+            all : ['Gruntfile.js']
+        },
+        watch : {
+            files : ['css/stylus/includes/*.styl', 'css/stylus/*.styl'],
+            tasks : ['stylus']
         }
-      }
-    }
-  });
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-  //loading stylus plugin
-  grunt.loadNpmTasks('grunt-contrib-stylus');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify', 'stylus']);
+    //loading stylus plugin
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+
+    //loading jshint plugin
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    
+    //loading watch plugin
+    grunt.loadNpmTasks('grunt-contrib-watch');    
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'stylus', 'jshint', 'watch']);
 
 };
