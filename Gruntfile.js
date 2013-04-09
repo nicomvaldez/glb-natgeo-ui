f6cec59b09fbb116d836f51dce434565d6bc4d5c
nicomvaldez <nicovaldez@ubuntu.ubuntu-domain>
2013-04-09 13:17:56 -0300

    minimizing backbone and underscore

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,12 @@ module.exports = function(grunt) {
             build : {
                 src : '<%= pkg.name %>.js',
                 dest : 'build/<%= pkg.name %>.min.js'
+            },
+            my_target : {
+                files : {
+                    'js/libs/backbone/backbone.min.js' : ['js/src/backbone/backbone.js'],
+                    'js/libs/underscore/underscore.min.js' : ['js/src/underscore/underscore.js']
+                }
             }
         },
         stylus : {
@@ -40,9 +46,9 @@ module.exports = function(grunt) {
 
     //loading jshint plugin
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    
+
     //loading watch plugin
-    grunt.loadNpmTasks('grunt-contrib-watch');    
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify', 'stylus', 'jshint', 'watch']);
